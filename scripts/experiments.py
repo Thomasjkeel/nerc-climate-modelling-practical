e84b1f5f2eef611718de0dc4e67d4c6d9e1fae60
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-02-15 17:51:59 +0000

    move across code

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -2,14 +2,18 @@ import os
 import numpy as np
 from lmfit import Model
 from model import surface_ocean_temp
+import matplotlib.pyplot as plt
 
-data_path = os.path.join('data','hadCRUT_data.txt')
+data_dir = data
+filename = 'hadCRUT_data.txt'
+
+data_path = os.path.join(data, filename)
 
 # array for time, in years and seconds
 t = np.array(range(0,171), dtype='int64')
 
-## get data
-HadCRUT_data = np.loadtxt(filename, delimiter=None, dtype=str) 
+## load in data (Move to get_data_func)
+HadCRUT_data = np.loadtxt(data_path, delimiter=None, dtype=str) 
 
 years = np.array([])
 temp_anom = np.array([])
@@ -37,3 +41,43 @@ params['alpha'].min = 0.68
 params['alpha'].max = 1.40
 
 result = mod.fit(temp_anom, params, t=t, method='least_squares')
+
+
+# quick plot of the temperature anomaly
+years = t + 1850
+
+plt.plot(years,temp_anom)
+plt.xlabel('Year')
+plt.xlim(1850,2025)
+plt.ylabel('Temperature Anomaly (K)')
+plt.show()
+
+result = mod.fit(temp_anom, params, t=t, method='least_squares')
+
+result.plot()
+plt.show()
+
+# Now make prediction for the next 100 years...
+
+t_fut = np.array(range(0,271), dtype='int64')
+years_fut = t_fut + 1850
+
+# best fit parameters from model
+
+A = result.params['A'].value
+B = result.params['B'].value
+F = result.params['F'].value
+alpha = result.params['alpha'].value
+
+
+T_fut = T(t=t_fut, A=A, B=B, F=F, alpha=alpha)
+
+plt.plot(years_fut, T_fut, label='model')
+plt.plot(years, temp_anom, label='HadCRUT data')
+
+plt.xlabel('Year', fontsize=12)
+plt.ylabel('Temperature Anomaly (K)', fontsize=12)
+
+plt.legend()
+
+plt.show()
