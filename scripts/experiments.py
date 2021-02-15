aa41c86e0fbfe8edca77de02eb10444bec13dce7
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-02-15 18:36:36 +0000

    add initial structure

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -1,83 +1,87 @@
 import os 
 import numpy as np
-from lmfit import Model
+import lmfit 
 from model import surface_ocean_temp
 import matplotlib.pyplot as plt
 
-data_dir = data
-filename = 'hadCRUT_data.txt'
-
-data_path = os.path.join(data, filename)
-
-# array for time, in years and seconds
-t = np.array(range(0,171), dtype='int64')
-
 ## load in data (Move to get_data_func)
-HadCRUT_data = np.loadtxt(data_path, delimiter=None, dtype=str) 
+def load_data(data_path):
+    ## TODO: will be extend to allow for getting climate model data on the fly
+    data = np.loadtxt(data_path, delimiter=None, dtype=str) 
+    return data
 
-years = np.array([])
-temp_anom = np.array([])
-for row in range(171):
-    years = np.append(years, float(HadCRUT_data[row][0]))
-    temp_anom = np.append(temp_anom, float(HadCRUT_data[row][1]))
+def calc_anomaly(data):
+    years = np.array([])
+    anom = np.array([])
+    for row in range(171):
+        years = np.append(years, float(data[row][0]))
+        anom = np.append(anom, float(data[row][1]))
 
-# convert to temperature anomaly from 1850
-temp_anom = temp_anom - temp_anom[0]
-
-
-# Now make a model that fits function for upper ocean temperature to the temperature anomaly data
-# in order to find parameters for function that give the best fit using least squares approach
-
-mod = Model(surface_ocean_temp)
-params = mod.make_params(A=-1,B=-1,F=2,alpha=1)
-
-# need to constrain the parameters as A + B = -F/alpha
-params['F'].expr = '-alpha*(A+B)'
-
-# Radiative forcing must be positive
-params['F'].min = 0
-# Alpha is 1.04+-0.36 from CMIP6 (in slides)
-params['alpha'].min = 0.68
-params['alpha'].max = 1.40
-
-result = mod.fit(temp_anom, params, t=t, method='least_squares')
+    # convert to temperature anomaly from 1850
+    anom = anom - anom[0]
+    return anom
 
 
-# quick plot of the temperature anomaly
-years = t + 1850
+def fit_model(data, model_used, t, **kwargs):
+    # Now make a model that fits function for upper ocean temperature to the temperature anomaly data
+    # in order to find parameters for function that give the best fit using least squares approach
+    
+    mod = lmfit.Model(model_used)
+    params = mod.make_params(**kwargs)
+    fitted_model = mod.fit(data, params, t=t, method='least_squares')
 
-plt.plot(years,temp_anom)
-plt.xlabel('Year')
-plt.xlim(1850,2025)
-plt.ylabel('Temperature Anomaly (K)')
-plt.show()
-
-result = mod.fit(temp_anom, params, t=t, method='least_squares')
-
-result.plot()
-plt.show()
-
-# Now make prediction for the next 100 years...
-
-t_fut = np.array(range(0,271), dtype='int64')
-years_fut = t_fut + 1850
-
-# best fit parameters from model
-
-A = result.params['A'].value
-B = result.params['B'].value
-F = result.params['F'].value
-alpha = result.params['alpha'].value
+    return fitted_model, params 
 
 
-T_fut = T(t=t_fut, A=A, B=B, F=F, alpha=alpha)
+def plot_model(years, model,  label, ax=None, fig=None):
+    if not ax:
+        fig, ax = plt.subplots(1)
+    plt.plot(years, model, label=label)
+    plt.xlabel('Year', fontsize=12)
+    plt.ylabel('Temperature Anomaly (K)', fontsize=12)
+    plt.legend()
+    return fig, ax
 
-plt.plot(years_fut, T_fut, label='model')
-plt.plot(years, temp_anom, label='HadCRUT data')
 
-plt.xlabel('Year', fontsize=12)
-plt.ylabel('Temperature Anomaly (K)', fontsize=12)
+def main():
+    ## GLOBALS
+    data_dir = '../data'
+    filename = 'hadCRUT_data.txt'
+    data_path = os.path.join(data_dir, filename)
+    # array for time, in years and seconds
+    t = np.array(range(0,171), dtype='int64')
+    years = t + 1850
+    t_fut = np.array(range(0,271), dtype='int64')
+    years_fut = t_fut + 1850
 
-plt.legend()
+    data_used = load_data(data_path)
+    temp_anom = calc_anomaly(data_used)
+    
+    fitted_model, params = fit_model(temp_anom, surface_ocean_temp, t=t, A=-1, B=-1, F=2, alpha=1)
 
-plt.show()
+    ## SET CONSTRAINTS
+    # need to constrain the parameters as A + B = -F/alpha
+    params['F'].expr = '-alpha*(A+B)'
+    # Radiative forcing must be positive
+    params['F'].min = 0
+    # Alpha is 1.04+-0.36 from CMIP6 (in slides)
+    params['alpha'].min = 0.68
+    params['alpha'].max = 1.40
+
+    # best fit parameters from model
+    A = fitted_model.params['A'].value
+    B = fitted_model.params['B'].value
+    F = fitted_model.params['F'].value
+    alpha = fitted_model.params['alpha'].value
+
+    projection = surface_ocean_temp(t=t_fut, A=A, B=B, F=F, alpha=alpha)
+
+    print(temp_anom)
+    ## plot and save ouputs
+    fig, ax = plot_model(years_fut, projection, label='model')
+    fig, ax = plot_model(years, temp_anom, label='HadCRUT data', fig=fig, ax=ax)
+    fig.savefig('../outputs/test.png', bbox_inches='tight', dpi=300)
+
+
+if __name__ == '__main__':
+    main()
