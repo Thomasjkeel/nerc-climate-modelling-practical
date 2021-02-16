c4b96c969ddff612ed58be8a4d4552c1f1878066
farrer <farr3r@github.com>
2021-02-16 11:34:03 +0000

    v simpe and correct model second go

--- scripts/model.py
+++ scripts/model.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 import os
 # set constants
-# data_dir = '../data'
-# ERF_data = pd.read_csv(os.path.join(data_dir, 'ERF_ssp585_1750-2500.csv'))
-# ERF_data = ERF_data.set_index('year')
-# ERF = ERF_data.loc[1850:2020]['total']
+#  data_dir = '../data'
+#  ERF_data = pd.read_csv(os.path.join(data_dir, 'ERF_ssp585_1750-2500.csv'))
+#  ERF_data = ERF_data.set_index('year')
+#  ERF = ERF_data.loc[1850:2020]['total']
 
 rho = 1000 # density of water kgm-3
 c_p = 4218 # specific heat of water Jkg-1K-1
@@ -28,11 +28,13 @@ dt = 365*24*60*60 # seconds in year
 # A and B are constants, where A + B = -F/alpha (from inhomogenous solution)
 
 def upper_ocean_temp(alpha, F):
+    # if type(F) != np.array and type(F) != np.ndarray:
+    #     F = ERF
     T_u = np.zeros(len(F))
     T_d = np.zeros(len(F))
 
     for i in range(len(F)-1):
-        T_u[i+1] = (1/C_u)*(ERF.iloc[i] - (alpha+gamma)*T_u[i] + T_d[i])*dt + T_u[i]
+        T_u[i+1] = (1/C_u)*(F[i] - (alpha+gamma)*T_u[i] + T_d[i])*dt + T_u[i]
         T_d[i+1] = (gamma/C_d)*(T_u[i]-T_d[i])*dt + T_d[i]
     
     return T_u
