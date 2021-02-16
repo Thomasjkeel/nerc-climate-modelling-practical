8e986338b3bdac6f7cb0ba98721e13f952284bf3
farrer <farr3r@github.com>
2021-02-16 11:29:43 +0000

    v simpe and correct model

--- scripts/model.py
+++ scripts/model.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 import os
 # set constants
-data_dir = '../data'
-ERF_data = pd.read_csv(os.path.join(data_dir, 'ERF_ssp585_1750-2500.csv'))
-ERF_data = ERF_data.set_index('year')
-ERF = ERF_data.loc[1850:2020]['total']
+# data_dir = '../data'
+# ERF_data = pd.read_csv(os.path.join(data_dir, 'ERF_ssp585_1750-2500.csv'))
+# ERF_data = ERF_data.set_index('year')
+# ERF = ERF_data.loc[1850:2020]['total']
 
 rho = 1000 # density of water kgm-3
 c_p = 4218 # specific heat of water Jkg-1K-1
@@ -19,27 +19,20 @@ gamma = (2*kap*c_p*rho)/(h_u+h_d) # prop constant for heat transfer to deep ocea
 C_u = rho*c_p*h_u # specific heat of upper ocean Jm-2K-1
 C_d = rho*c_p*h_d # specific heat of deep ocean Jm-2K-1
 
+dt = 365*24*60*60 # seconds in year
+
 
 # Solved second order differential equation to find expression for T_u:
 # T_u = Aexp(lambda1*t) + Bexp(lambda2*t) + F/alpha
 # where lambda1,2 are found using quadratic formula from homogenous 2nd order ODE solution, and
 # A and B are constants, where A + B = -F/alpha (from inhomogenous solution)
 
-def surface_ocean_temp(t, A, B, alpha, F=None):
-    # function for upper ocean temperature, T_u, with variable inputs for constants A, B, F and alpha
-    if type(F) != np.array and type(F) != np.ndarray:
-        F = ERF
+def upper_ocean_temp(alpha, F):
+    T_u = np.zeros(len(F))
+    T_d = np.zeros(len(F))
 
-    # terms for quadratic equation to solve second order ODE solution for T_u
-    a = C_u/gamma
-    b = (alpha+gamma)/gamma + C_u/C_d
-    c = (alpha+gamma)/C_d - gamma/C_d
+    for i in range(len(F)-1):
+        T_u[i+1] = (1/C_u)*(ERF.iloc[i] - (alpha+gamma)*T_u[i] + T_d[i])*dt + T_u[i]
+        T_d[i+1] = (gamma/C_d)*(T_u[i]-T_d[i])*dt + T_d[i]
     
-    # exponential coefficients for T_u solution using quadratic formula
-    lambda1 = (-b + np.sqrt(b**2 - 4*a*c))/(2*a)
-    lambda2 = (-b - np.sqrt(b**2 - 4*a*c))/(2*a)
-    
-    t_s = t*365*24*60*60 # convert time to seconds
-    
-    T_u = A * np.exp(lambda1*t_s) + B * np.exp(lambda2*t_s) + F/alpha
     return T_u
