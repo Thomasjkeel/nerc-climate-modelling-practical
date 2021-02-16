a44b1d4f5f1d6b3fe61b2c6d85d1c3abd2518080
farrer <farr3r@github.com>
2021-02-16 12:14:08 +0000

    opt alpha func

--- scripts/model.py
+++ scripts/model.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 import os
 # set constants
-#  data_dir = '../data'
-#  ERF_data = pd.read_csv(os.path.join(data_dir, 'ERF_ssp585_1750-2500.csv'))
-#  ERF_data = ERF_data.set_index('year')
-#  ERF = ERF_data.loc[1850:2020]['total']
+data_dir = '../data'
+ERF_data = pd.read_csv(os.path.join(data_dir, 'ERF_ssp585_1750-2500.csv'))
+ERF_data = ERF_data.set_index('year')
+ERF = ERF_data.loc[1850:2020]['total']
 
 rho = 1000 # density of water kgm-3
 c_p = 4218 # specific heat of water Jkg-1K-1
@@ -27,14 +27,22 @@ dt = 365*24*60*60 # seconds in year
 # where lambda1,2 are found using quadratic formula from homogenous 2nd order ODE solution, and
 # A and B are constants, where A + B = -F/alpha (from inhomogenous solution)
 
-def upper_ocean_temp(alpha, F):
-    # if type(F) != np.array and type(F) != np.ndarray:
-    #     F = ERF
-    T_u = np.zeros(len(F))
-    T_d = np.zeros(len(F))
+def upper_ocean_temp(t, alpha, F=None):
+    if type(F) != np.array and type(F) != np.ndarray:
+        F = ERF
+    T_u = np.zeros(t)
+    T_d = np.zeros(t)
 
-    for i in range(len(F)-1):
+    for i in range(t-1):
         T_u[i+1] = (1/C_u)*(F[i] - (alpha+gamma)*T_u[i] + T_d[i])*dt + T_u[i]
         T_d[i+1] = (gamma/C_d)*(T_u[i]-T_d[i])*dt + T_d[i]
     
     return T_u
+
+
+def opt_alpha(temp_anom, t=171):
+    mod = lmfit.Model(upper_ocean_temp)
+    params = mod.make_params(alpha=1)
+    fit_result = mod.fit(temp_anom, params, t=t)
+    
+    return result.params['alpha'].value
