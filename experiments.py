ee98282183888aa49a709efceeda1d8f8ea8b9dd
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-02-16 14:48:13 +0000

    add some experiments

--- experiments.py
+++ experiments.py
@@ -43,6 +43,16 @@ def load_forcing_data(filename):
     ERF_fut = np.array(ERF_data.loc[1850:2100]['total'] * FORCING_SENSITIVITY)
     return ERF, ERF_fut
 
+
+def calc_confidence_interval(data):
+    n = len(data)
+    x_bar = data.mean()
+    st_dev = np.std(data) 
+    upper_conf_int = x_bar + 1.960 * st_dev/np.sqrt(n)
+    lower_conf_int = x_bar - 1.960 * st_dev/np.sqrt(n)
+    return upper_conf_int, lower_conf_int
+
+
 def main():
     # array for time, in years and seconds
     t = np.array(range(0,171), dtype='int64')
@@ -61,20 +71,29 @@ def main():
     model_data_used = load_data(data_path)
     temp_anom = calc_anomaly(model_data_used, num_years=171)
     
-    opt_alpha_val = model.opt_alpha(temp_anom)
-
     ## initialise_plot
     fig, ax = plt.subplots(1, figsize=(10,10))
     fig, ax = plot_model(years, temp_anom, label='HadCRUT temperature  anomaly', fig=fig, ax=ax)
 
     ## run model under different forcing scenarios
     for scen_file in os.listdir(path_to_ssp_forcings):
+
         forcing_scenario_path = os.path.join(path_to_ssp_forcings, scen_file)
         ERF, ERF_fut = load_forcing_data(forcing_scenario_path)
-        projection = model.upper_ocean_temp(t=len(ERF_fut), alpha=opt_alpha_val, F=ERF_fut)
+        alpha_val, alpha_stderr = model.get_opt_model(temp_anom=temp_anom, F=ERF)
+        # upper_alpha, lower_alpha = calc_confidence_interval(alpha_val, alpha_stderr)
 
+        projection = model.upper_ocean_temp(t=len(ERF_fut), alpha=alpha_val, F=ERF_fut)
+        upper_conf, lower_conf = calc_confidence_interval(projection)
+        # upper_alpha_proj = model.upper_ocean_temp(t=len(ERF_fut), alpha=upper_alpha, F=ERF_fut)
+        # lower_alpha_proj = model.upper_ocean_temp(t=len(ERF_fut), alpha=lower_alpha, F=ERF_fut)
         ## plot and save ouputs
         fig, ax = plot_model(years_fut, projection, label='%s' % (scen_file[:-4]), fig=fig, ax=ax)
+        # fig, ax = plot_model(years_fut, projection+upper_conf, label='%s upper' % (scen_file[:-4]), fig=fig, ax=ax)
+        # fig, ax = plot_model(years_fut, projection+lower_conf, label='%s lower' % (scen_file[:-4]), fig=fig, ax=ax)
+        # fig, ax = plot_model(years_fut, projection+(2*st_dev), label='%s st_dev above' % (scen_file[:-4]), fig=fig, ax=ax)
+        # fig, ax = plot_model(years_fut, projection-(2*st_dev), label='%s st_dev below' % (scen_file[:-4]), fig=fig, ax=ax)
+        break
     fig.savefig('outputs/upper_ocean_projection.png', bbox_inches='tight', dpi=300)
 
 
